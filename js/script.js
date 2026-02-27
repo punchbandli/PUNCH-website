933e7fb87307f17e0a3bf59dd33d56e9663375f6
punchbandli <punchbandli@gmail.com>
2026-02-26 22:28:12 -0500

    Add smooth scroll functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-document.addEventListener('DOMContentLoaded',()=>console.log('PUNCH Band website loaded successfully!'));
+'use strict';
+
+// Smooth scrolling functionality
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
